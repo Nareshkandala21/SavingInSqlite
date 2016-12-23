5cd247bb6565d28d761fa73a38b3929f0490456b
Nareshkandala21 <nareshkandala.ios@gmail.com>
2016-12-23 19:28:03 +0530

    Initial Commit

---
+++ SampleSqlite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleSqlite
+//
+//  Created by Naresh Kandala on 23/12/16.
+//  Copyright © 2016 Naresh Kandala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
